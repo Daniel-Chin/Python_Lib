0350e7695059242e7ca071814a9f5d1f8e957107
Daniel Chin <daniel_chin@yahoo.com>
2018-03-02 10:46:42 +0800

    Auto upload

--- console_explorer.py
+++ console_explorer.py
@@ -84,7 +84,78 @@ def askForFile(cd = None):
             else:
                 print('Not a dir. ')
         elif op == 'help':
-            printHelp()
+            print('Enter to next')
+            print('"e" to enter')
+            print('"s" to search')
+            print('num to id')
+            print('"abort", "abs", "cls"')
+        elif op == 'abort':
+            return None
+        else:
+            try:
+                id = int(op)
+                if id in range(len(track.list)):
+                    track.selected = id
+            except:
+                print('No such command. ')
+
+def askSaveWhere(cd = None, initialfile = None):
+    if cd is None:
+        if platform.system() == 'Linux':
+            cd = '/storage/emulated/0/' 
+        else:
+            cd = os.getcwd()
+    stop = False
+    track = Track(cd)
+    while not stop:
+        print('='*10)
+        printTrack(track)
+        print()
+        print('Now selected:', track.list[track.selected].name)
+        op = input(cd + '>').lower()
+        if op == '':
+            track.down()
+        elif op == 'e':
+            item = track.list[track.selected]
+            if type(item) in (Parent, Dir):
+                cd = path.abspath(path.join(cd, item.name))
+                track = Track(cd)
+            elif type(item) is File:
+                print('It is a file, not a dir. ')
+            else:
+                assert False
+        elif op == 'this':
+            filename = None
+            if initialfile is not None:
+                print('Do you want to use', initialfile, 'as filename?')
+                op = input('y/n >').lower()
+                if op == 'y':
+                    filename = initialfile
+            if filename is None:
+                filename = input('Save as filename: ')
+            return path.join(cd, filename)
+        elif op == 's':
+            kw = input('keyword: ')
+            matches = [x for x in track.list if kw.lower() in x.name.lower()]
+            choosed = chooseFromEntries(matches)
+            if choosed is not None:
+                track.selected = track.list.index(choosed)
+        elif op == 'cls':
+            cls()
+        elif op == 'abs':
+            new_cd = input('Abs path: ')
+            if path.isdir(new_cd):
+                cd = new_cd
+                track = Track(cd)
+            else:
+                print('Not a dir. ')
+        elif op == 'help':
+            print('Enter to next')
+            print('"e" to enter')
+            print('"this" to return cd')
+            print('"s" to search')
+            print('num to id')
+            print('"abort", "abs", "cls"')
         elif op == 'abort':
             return None
         else:
@@ -114,13 +185,6 @@ def chooseFromEntries(matches):
             print("Search aborted. ")
             return None
 
-def printHelp():
-    print('Enter to next')
-    print('"e" to enter')
-    print('"s" to search')
-    print('num to id')
-    print('"abort", "abs", "cls"')
-
 def printTrack(track):
     form = str(len(str(len(track.list))))
     for i, item in enumerate(track.list):
@@ -139,5 +203,5 @@ def printTrack(track):
             assert False
 
 if __name__ == '__main__':
-    print(askForFile())
+    print(askSaveWhere('d:/', 'a.jpg'))
     input('enter...')
