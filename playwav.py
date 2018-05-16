b7d153aa2c0497f34a489f16723cd499d01fc162
秦Daniel <daniel_chin@yahoo.com>
2018-05-16 18:14:25 +0800

    Auto upload

---
+++ playwav.py
@@ -0,0 +1,13 @@
+import sys
+import simpleaudio as sa
+from os import path
+
+try:
+    if path.isfile(sys.argv[1]):
+        sa.WaveObject.from_wave_read(sa.wave.open(sys.argv[1])).play()
+    else:
+        input('File doesn\'t exist. ')
+except IndexError:
+    input('Please open a file with this app. ')
+input('Enter to quit...')
+sys.exit(0)
--- stats.py
+++ stats.py
@@ -1,18 +1,24 @@
 def mean(sample):
     return sum(sample)/len(sample)
 
-def var(sample, fix = True):
+def var(sample, fix = True, clever = True):
     sample_mean = mean(sample)
-    sum = 0
-    for i in sample:
-        sum += (i - sample_mean) ** 2
+    if clever:
+        sum = 0
+        for i in sample:
+            sum += i**2
+        sum -= sample_mean ** 2 * len(sample)
+    else:
+        sum = 0
+        for i in sample:
+            sum += (i - sample_mean) ** 2
     if fix:
         return sum / (len(sample) - 1)
     else:
         return sum / len(sample)
 
-def std(sample, fix = True):
-    return var(sample, fix) ** .5
+def std(*args, **kw):
+    return var(*args, **kw) ** .5
 
 def inputList():
     new_list = []
