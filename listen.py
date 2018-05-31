1e5483acb196f93be421fa051f0660c76ff35e83
秦Daniel <daniel_chin@yahoo.com>
2018-05-31 12:54:51 +0800

    Auto upload

--- listen.py
+++ listen.py
@@ -7,56 +7,61 @@ if 'flush' not in getargspec(print).args:
     def print(*args, flush = False, **kw):
         print_3(*args, **kw)
 '''
+from time import sleep
+FPS = 30
 
 try:
     import msvcrt
-    from time import sleep
-    
-    def GetFullCh():
+    import sys
+    def getFullCh():
         first=msvcrt.getch()
         if first in (b'\x00', b'\xe0'):
             return first + msvcrt.getch()
         else:
             return first
-    
-    def listen(choice=None,timeout=0):
-        '''
-        choice can be a list of choices or a single choice. 
-        Elements can be b'' or ''.
-        If timeout=0, it's blocking. 
-        timeout is in second. 
-        '''
-        if choice is not None:
-            if type(choice) in (bytes, str):
-                choice = (choice, )
-            bChoice=[]
-            for i in choice:
-                if type(i) is bytes:
-                    bChoice.append(i)
-                else:
-                    bChoice.append(i.encode())
-        print('',end='',flush=True)
-        if timeout != 0:
-            fps=30
-            for i in range(int(timeout*fps)):
-                if msvcrt.kbhit():
-                    break
-                sleep(1/fps)
-            else:
-                return None
-        op=GetFullCh()
-        while not (choice is None or op in bChoice):
-            op=GetFullCh()
-        return op
+    if sys.getwindowsversion() >= (10, 0, 17134):
+        __getFullCh = getFullCh
+        def getFullCh():
+            ch = __getFullCh()
+            if len(ch) == 1:
+                assert msvcrt.getch() == b'\x00'
+            return ch
 
 except ImportError:
-    def listen(choice=None,timeout=0):
+    msvcrt = None
+
+def listen(choice=None, timeout=0):
+    '''
+    choice can be a list of choices or a single choice. 
+    Elements can be b'' or ''.
+    If timeout=0, it's blocking. 
+    timeout is in second. 
+    '''
+    if msvcrt is None:
         print(choice)
-        op = input()
-        if '\\x' in op:
-            return chr(int(op.split('\\x')[1], 16)).encode()
-        else:
-            return op.encode()
+        return eval('"' + input() + '"').encode()
+    if choice is not None:
+        if type(choice) in (bytes, str):
+            choice = (choice, )
+        bChoice=[]
+        for i in choice:
+            if type(i) is bytes:
+                bChoice.append(i)
+            else:
+                bChoice.append(i.encode())
+    print('',end='',flush=True)     # Just to flush
+    if timeout != 0:
+        for i in range(int(timeout*FPS)):
+            if msvcrt.kbhit():
+                op = getFullCh()
+                if choice is None or op in bChoice:
+                    return op
+            sleep(1/FPS)
+        return None
+    op=getFullCh()
+    while not (choice is None or op in bChoice):
+        op=getFullCh()
+    return op
 
 if __name__=='__main__':
     from console import console
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
+dict_Z = {.05  : 1.645, 
+          .025 : 1.96, 
+          .01  : 2.326, 
+          .005 : 2.576, 
+          .0005: 3.291
+         }
+
+def Z(half_alpha):
+    half_alpha = round(half_alpha * 1000000) / 1000000
+    return dict_Z[half_alpha]
+
 def mean(sample):
     return sum(sample)/len(sample)
 
@@ -32,6 +43,23 @@ def inputList():
     while takeInput():
         new_list.append(float(op))
     return new_list
+
+def compare(sample_1, sample_2, confidence = .95):
+    assert len(sample_1) >= 30 and len(sample_2) >= 30
+    delta = mean(sample_1) - mean(sample_2)
+    s = (var(sample_1) / len(sample_1) + var(sample_2) / len(sample_2))**.5
+    return confidenceInterval(delta, s, .99, Z)
+
+def confidenceInterval(point_est, deviation, confidence = .95, distribution = Z):
+    return (point_est - marginError(deviation, confidence, distribution), \
+        point_est + marginError(deviation, confidence, distribution))
+
+def marginError(deviation, confidence = .95, distribution = Z, two_tail = True):
+    if two_tail:
+        half_alpha = (1 - confidence) / 2
+    else:
+        half_alpha = (1 - confidence)
+    return deviation * distribution(half_alpha)
 
 if __name__ == '__main__':
     from console import console
