db2b0715856d32c4e748b28adf9992c17de0657d
NYU User <nyuuser@NYUSH0219LP-MX.local>
2018-07-02 12:36:53 +0800

    Auto upload

--- OneShip.py
+++ OneShip.py
@@ -16,40 +16,44 @@ port=2337
 def main():
     s=PickleSocket()
     print('My IP =',gethostbyname(gethostname()))
-    print('Server or Client? s/c')
-    if listen((b's',b'c')) == b's':
-        s.bind(('',port))
-        s.listen(1)
-        print('Listening...')
-        server = s
-        s,addr=s.accept()
-        print('Connection from',addr)
-        print('Accept? y/n')
-        if listen((b'y',b'n')) == b'n':
-            s.close()
-            input('Aborted. Enter to exit...')
-            sys.exit(1)
-    else:
-        ip=input('Connect to IP: ')
-        print('Connecting...')
-        s.connect((ip,port))
-    print('Connected! ')
-    s.shakeHands('One ship python by Daniel Chin. ')
-    
-    esc=b'\x1b'
-    op=b''
-    while op != esc:
-        if op==b's':
-            send(s)
-        elif op==b'r':  #Cannot use ELSE, cuz op maybe esc. 
-            recv(s)
-        print('Send, receive, or exit? s/r/esc')
-        op=listen((b's',b'r',esc))
-    s.close()
+    server = None
+    s = None
     try:
-        server.close()
-    except:
-        pass
+        print('Server or Client? s/c')
+        if listen((b's',b'c')) == b's':
+            s.bind(('',port))
+            s.listen(1)
+            print('Listening...')
+            server = s
+            s,addr=server.accept()
+            print('Connection from',addr)
+            print('Accept? y/n')
+            if listen((b'y',b'n')) == b'n':
+                s.close()
+                s = None
+                input('Aborted. Enter to exit...')
+                sys.exit(1)
+        else:
+            ip=input('Connect to IP: ')
+            print('Connecting...')
+            s.connect((ip,port))
+        print('Connected! ')
+        s.shakeHands('One ship python by Daniel Chin. ')
+        
+        esc=b'\x1b'
+        op=b''
+        while op != esc:
+            if op==b's':
+                send(s)
+            elif op==b'r':  #Cannot use ELSE, cuz op maybe esc.
+                recv(s)
+            print('Send, receive, or exit? s/r/esc')
+            op=listen((b's',b'r',esc))
+    finally:
+        if s:
+            s.close()
+        if server:
+            server.close()
 
 def send(s):
     if Tk:
