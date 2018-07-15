3706a53bd38278d17d8a18a7001ef3c205140f43
Daniel Chin <daniel_chin@yahoo.com>
2018-07-15 14:31:03 +0800

    Auto upload

---
+++ cache_no_hash.py
@@ -0,0 +1,14 @@
+class cache:
+    def __init__(self, func):
+        self.func = func
+        self.i = []
+        self.o = []
+    
+    def __call__(self, *args, **kw):
+        for i, trying in enumerate(self.i):
+            if trying == (args, kw):
+                return self.o[i]
+        self.i.append((args, kw))
+        result = self.func(*args, **kw)
+        self.o.append(result)
+        return result
