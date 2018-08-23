8bcc305658e1461459229d04884f490db60f7706
秦Daniel <daniel_chin@yahoo.com>
2018-08-23 16:23:51 +0800

    Auto upload

--- recvfile.py
+++ recvfile.py
@@ -4,7 +4,7 @@ from socket import socket as Socket
 def recvFile(socket,file_len,to_filename):
     assert type(socket) is Socket
     j=jdt.CommJdt(file_len,msg=to_filename[-6:])
-    with open(to_filename,'wb') as to_file:
+    with open(to_filename,'wb+') as to_file:
         left=file_len
         while left:
             for i in range(128):
