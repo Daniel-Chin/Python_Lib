82465015592eb234c6ebbccee58c8d07078e5983
秦Daniel <daniel_chin@yahoo.com>
2018-09-13 14:27:33 +0800

    Auto upload

--- console.py
+++ console.py
@@ -12,7 +12,7 @@ from kernal import Kernal
 
 CURSOR = '|'
 
-def console(namespace = {}, prompt = '>>> '):
+def console(namespace = {}, prompt = '>>> ', use_input = False, fixer = None):
     if platform.system() != 'Windows':
         from IPython import embed
         embed()
@@ -25,85 +25,93 @@ def console(namespace = {}, prompt = '>>> '):
         command = ''
         cursor = 0
         history_selection = len(history)
-        print(prompt + CURSOR, end='\r')
-        while True:
-            op = listen(timeout = .5)
-            last_len = len(command)
-            if op == b'\r':
-                print(prompt + command.replace('\x1a', '^Z') + ' ')
-                break
-            elif op is None:
-                cursor_bright = not cursor_bright
-            else:
-                cursor_bright = True
-                if op == b'\x08':
-                    if cursor >= 1:
-                        command = command[:cursor - 1] + command[cursor:]
-                        cursor -= 1
-                elif op == b'\xe0S':
-                    if cursor <= len(command):
-                        command = command[:cursor] + command[cursor + 1:]
-                elif op == b'\xe0H':
-                    history_selection -= 1
-                    if history_selection in range(len(history)):
-                        command = history[history_selection]
-                        cursor = len(command)
-                    else:
-                        history_selection +=1
-                elif op == b'\xe0P':
-                    history_selection += 1
-                    if history_selection in range(len(history)):
-                        command = history[history_selection]
-                        cursor = len(command)
-                    else:
-                        history_selection -=1
-                elif op == b'\xe0K':
-                    cursor -= 1
-                    if cursor not in range(len(command) + 1):
-                        cursor += 1
-                elif op == b'\xe0M':
-                    cursor += 1
-                    if cursor not in range(len(command) + 1):
-                        cursor -= 1
-                elif op == b'\xe0G':
-                    cursor = 0
-                elif op == b'\xe0O':
-                    cursor = len(command)
-                elif op == b'\xe0s': # Ctrl Left
-                    word_started = False
-                    for cursor in range(cursor - 1, -1, -1):
-                        if command[cursor].isalpha():
-                            word_started = True
-                        else:
-                            if word_started:
-                                cursor += 1
-                                break
-                elif op == b'\xe0t': # Ctrl Right
-                    word_started = False
-                    word_ended = False
-                    for cursor in range(cursor, len(command)):
-                        if command[cursor].isalpha():
-                            if word_ended:
-                                break
-                            else:
-                                word_started = True
-                        else:
-                            if word_started:
-                                word_ended = True
-                    else:
-                        cursor = len(command)
-                elif op[0] in range(1, 26) or op[0] in (0, 224):
-                    pass
+        if use_input:
+            try:
+                command = input(prompt)
+            except EOFError:
+                command = 'exit()'
+        else:
+            print(prompt + CURSOR, end='\r')
+            while True:
+                op = listen(timeout = .5)
+                last_len = len(command)
+                if op == b'\r':
+                    print(prompt + command.replace('\x1a', '^Z') + ' ')
+                    break
+                elif op is None:
+                    cursor_bright = not cursor_bright
                 else:
-                    command = command[:cursor] + op.decode() + command[cursor:]
-                    cursor += 1
-            padding = max(0, last_len - len(command)) * 2
-            if cursor_bright:
-                cursor_show = CURSOR
-            else:
-                cursor_show = '_'
-            cursed_command = command[:cursor] + cursor_show + command[cursor:]
-            print(prompt + cursed_command.replace('\x1a', '^Z') + ' '*padding, end='\r')
+                    cursor_bright = True
+                    if op == b'\x08':
+                        if cursor >= 1:
+                            command = command[:cursor - 1] + command[cursor:]
+                            cursor -= 1
+                    elif op == b'\xe0S':
+                        if cursor <= len(command):
+                            command = command[:cursor] + command[cursor + 1:]
+                    elif op == b'\xe0H':
+                        history_selection -= 1
+                        if history_selection in range(len(history)):
+                            command = history[history_selection]
+                            cursor = len(command)
+                        else:
+                            history_selection +=1
+                    elif op == b'\xe0P':
+                        history_selection += 1
+                        if history_selection in range(len(history)):
+                            command = history[history_selection]
+                            cursor = len(command)
+                        else:
+                            history_selection -=1
+                    elif op == b'\xe0K':
+                        cursor -= 1
+                        if cursor not in range(len(command) + 1):
+                            cursor += 1
+                    elif op == b'\xe0M':
+                        cursor += 1
+                        if cursor not in range(len(command) + 1):
+                            cursor -= 1
+                    elif op == b'\xe0G':
+                        cursor = 0
+                    elif op == b'\xe0O':
+                        cursor = len(command)
+                    elif op == b'\xe0s': # Ctrl Left
+                        word_started = False
+                        for cursor in range(cursor - 1, -1, -1):
+                            if command[cursor].isalpha():
+                                word_started = True
+                            else:
+                                if word_started:
+                                    cursor += 1
+                                    break
+                    elif op == b'\xe0t': # Ctrl Right
+                        word_started = False
+                        word_ended = False
+                        for cursor in range(cursor, len(command)):
+                            if command[cursor].isalpha():
+                                if word_ended:
+                                    break
+                                else:
+                                    word_started = True
+                            else:
+                                if word_started:
+                                    word_ended = True
+                        else:
+                            cursor = len(command)
+                    elif op[0] in range(1, 26) or op[0] in (0, 224):
+                        pass
+                    else:
+                        command = command[:cursor] + op.decode() + command[cursor:]
+                        cursor += 1
+                padding = max(0, last_len - len(command)) * 2
+                if cursor_bright:
+                    cursor_show = CURSOR
+                else:
+                    cursor_show = '_'
+                cursed_command = command[:cursor] + cursor_show + command[cursor:]
+                print(prompt + cursed_command.replace('\x1a', '^Z') + ' '*padding, end='\r')
+        if fixer is not None:
+            command = fixer(command)
         if command in ('exit', 'exit()', '\x1a'):
             try:
                 kernal.send('exit')
---
+++ formula.py
@@ -0,0 +1,24 @@
+'''
+Evaluate beautiful formulas like 	√(2×3)
+'''
+from console import console
+from math import sqrt
+
+def fixer(raw):
+    s1 = raw.replace('×', '*').replace('√(', 'sqrt(')
+    l1 = s1.split('√')
+    for i_seg in range(1, len(l1)):
+        for i, chara in enumerate(l1[i_seg]):
+            if chara=='.' or chara.isnumeric():
+                continue
+            i -= 1
+            break
+        i += 1
+        l1[i_seg] = 'sqrt(' + l1[i_seg][:i] + ')' + l1[i_seg][i:]
+    return ''.join(l1)
+
+def main():
+    console({'sqrt':sqrt}, use_input = True, fixer = fixer)
+
+if __name__ == '__main__':
+    main()
