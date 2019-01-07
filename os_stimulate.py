f768b5f5b0e77d0d0ecd75ae7b3e67ee92a17562
秦Daniel <daniel_chin@yahoo.com>
2019-01-06 22:29:08 -0500

    Auto upload

--- os_stimulate.py
+++ os_stimulate.py
@@ -2,6 +2,9 @@
 My laptop is weird. Running this script makes opening and terminating processes faster. 
 Speculation: Taking CPU time away from Windows Defender? 
 '''
+DEFAULT_INTERVAL = 5
+print('Interval =', DEFAULT_INTERVAL, end = '\r')
+print('Interval = ', end = '', flush = True)
 from subprocess import Popen
 from time import sleep
 from os import kill
@@ -9,13 +12,24 @@ from os import kill
 SIGKILL: int = 9
 
 def main(): 
+    try: 
+        interval: int = int(input())
+        assert interval > 0
+    except:
+        interval = DEFAULT_INTERVAL
     while True:
         print('open')
         p: Popen = Popen('taskmgr')
-        sleep(5)
+        sleepLoud(interval)
         print('kill')
         kill(p.pid, SIGKILL)
-        sleep(2)
+        sleepLoud(2)
+
+def sleepLoud(length):
+    assert type(length) is int
+    for i in range(length):
+        print(i, end = '', flush = True)
+        sleep(1)
 
 if __name__ == '__main__':
     main()
