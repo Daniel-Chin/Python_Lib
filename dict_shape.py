a49ac609f357b295750bc0313ff3bc5fe976e3c0
秦Daniel <daniel_chin@yahoo.com>
2019-01-15 09:01:10 -0500

    Auto upload

---
+++ dict_shape.py
@@ -0,0 +1,20 @@
+'''
+Compare the shape of two dicts. 
+i.e. is the structure the same? are the keys the same? 
+'''
+import json
+
+def shapeOfDict(x):
+    y = {}
+    for key, value in x.items():
+        if type(value) is dict:
+            y[key] = shapeOfDict(value)
+        else:
+            y[key] = None
+    return y
+
+def dictShapeCompare(a, b):
+    return shapeOfDict(a) == shapeOfDict(b)
+
+def pprintDict(x):
+    print(json.dumps(x, indent = 2))
