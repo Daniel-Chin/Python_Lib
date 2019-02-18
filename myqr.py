f5a65d70b0ec28ec49a8569ce605fc4e2a735342
秦Daniel <daniel_chin@yahoo.com>
2019-02-18 18:14:25 +0800

    Auto upload

--- myqr.py
+++ myqr.py
@@ -2,18 +2,29 @@ import qrcode
 from colorama import Fore, Back, Style, init
 init()
 
-def printQR(data = None, invert = True):
+def printQR(data = None):
     qrCode = qrcode.main.QRCode()
     qrCode.add_data(data)
     matrix = qrCode.get_matrix()
-    for row in matrix:
+    if len(matrix) % 2 == 1:
+        matrix.append(matrix[0])
+    iter_matrix = iter(matrix)
+    for row_up in iter_matrix:
+        row_down = next(iter_matrix)
         line = []
-        for pixel in row:
-            if pixel ^ invert:
-                line.append(Back.WHITE)
+        for up, down in zip(row_up, row_down):
+            if up ^ down:
+                char = '▄'
             else:
-                line.append(Back.BLACK)
-        print(*line, end = '  \n', sep = '  ')
+                char = '⃪'
+            if up:
+                line.append(Fore.WHITE)
+                line.append(Back.LIGHTBLACK_EX)
+            else:
+                line.append(Back.LIGHTWHITE_EX)
+                line.append(Fore.BLACK)
+            line.append(char)
+        print(*line, sep = '')
     print(Style.RESET_ALL, end = '')
 
 if __name__ == '__main__':
