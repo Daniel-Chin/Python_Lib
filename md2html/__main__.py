853d3d44d5657688ab9b8c441b9d17088d05c87b
秦Daniel <daniel_chin@yahoo.com>
2019-03-16 00:48:46 +0800

    Auto upload

--- md2html/__main__.py
+++ md2html/__main__.py
@@ -3,8 +3,89 @@ Translate markdown .md file to html file.
 Note: to work on WordPress, avoid level one header (# Title).  
 '''
 import argparse
+import re
+import sys
+from itertools import cycle
 
 FONT_SIZE = (12, 36, 24, 18, 16, 14, 13, 13, 13)
+
+class Translator:
+    def __init__(self):
+        self.code_block_started = False
+    
+    def translateLine(self, src):
+        s = self.handleCode(src.rstrip())
+        if self.code_block_started:
+            tag = '%s<br />'
+            s = s.replace(' ' * 2, '&nbsp;' * 2)
+        else:
+            n_hash = 0
+            while s.startswith('#'):
+                n_hash += 1
+                s = s[1:]
+            tag = self.n_hash2Tag(n_hash)
+            s = self.handleLinks(s)
+            s += '&nbsp;'
+        return tag % s
+    
+    def n_hash2Tag(self, n_hash):
+        styles = []
+        styles.append('font-size: %dpt;' % FONT_SIZE[n_hash])
+        # styles.append('line-height: 1.2;')
+        if n_hash >= 1:
+            styles.append('margin-top: 0;')
+            styles.append('padding-top: 2pt;')
+            styles.append('padding-bottom: 2pt;')
+            styles.append('margin-bottom: 0;')
+            styles.append('font-weight: bold;')
+        style = 'style="%s"' % ' '.join(styles)
+        if n_hash == 0:
+            tag = f'<span {style}>%s</span><br />'
+        if n_hash >= 1:
+            tag = f'<h{n_hash} {style}>%s</h{n_hash}>'
+            if n_hash in (1, 2):
+                tag += '<hr />'
+        return tag
+    
+    def handleLinks(self, s):
+        # before[text](link)after
+        try:
+            while '](' in s:
+                left, right = s.split('](', 1)
+                brack_pos = left.rfind('[')
+                before = left[:brack_pos]
+                text = left[brack_pos + 1:]
+                link, after = right.split(')', 1)
+                s = '%s<a href="%s">%s</a>%s' % (before, link, text, after)
+        except Exception as e:
+            print(e, 'illegal line:', s)
+        return s
+    
+    def handleCode(self, s):
+        backticks = [x.end() for x in re.finditer('```', s)]
+        if backticks:
+            if len(backticks) >= 2:
+                print('Error: two "```"s in one line. ')
+                print('Problem line:', src)
+                sys.exit(1)
+            if self.code_block_started:
+                s = s.replace('```', '</code>')
+            else:
+                language = s[backticks[0]:].strip()
+                if language:
+                    tag = '<code class="language-%s">' % language
+                else:
+                    tag = '<code>'
+                s = s[:backticks[0] - 3] + tag
+            self.code_block_started = not self.code_block_started
+        if '`' in s:    # code segment
+            parts = s.split('`')
+            if len(parts) % 2 == 0:
+                print('Error: odd number of ` in one line. ')
+                print('Problem line:', src)
+                sys.exit(1)
+            s = ''.join([x + y for x, y in zip(parts[:-1], cycle(['<code>', '</code>']))]) + parts[-1]
+        return s
 
 def main():
     args = parseArgs()
@@ -15,8 +96,9 @@ def main():
         write('<html><body>')
         try:
             with open(args.input, 'r') as md:
+                translator = Translator()
                 for line in md:
-                    write(translateLine(line))
+                    write(translator.translateLine(line))
         except FileNotFoundError:
             print('Please run python -m md2html -h')
             return
@@ -29,48 +111,6 @@ def parseArgs():
     parser.add_argument('--output', '-o', type = str, default = 'build.html', help = 'output html file. Default: build.html')
     return parser.parse_args()
 
-def translateLine(src):
-    n_hash = 0
-    while src.startswith('#'):
-        n_hash += 1
-        src = src[1:]
-    tag = n_hash2Tag(n_hash)
-    s = src.strip() + '&nbsp;'
-    s = handleLinks(s)
-    return tag % s
-
-def n_hash2Tag(n_hash):
-    styles = []
-    styles.append('font-size: %dpt;' % FONT_SIZE[n_hash])
-    # styles.append('line-height: 1.2;')
-    if n_hash >= 1:
-        styles.append('margin-top: 0;')
-        styles.append('padding-top: 2pt;')
-        styles.append('padding-bottom: 2pt;')
-        styles.append('margin-bottom: 0;')
-        styles.append('font-weight: bold;')
-    style = 'style="%s"' % ' '.join(styles)
-    if n_hash == 0:
-        tag = f'<span {style}>%s</span><br />'
-    if n_hash >= 1:
-        tag = f'<h{n_hash} {style}>%s</h{n_hash}>'
-        if n_hash in (1, 2):
-            tag += '<hr />'
-    return tag
-
-def handleLinks(s):
-    # before[text](link)after
-    try:
-        while '](' in s:
-            left, right = s.split('](', 1)
-            brack_pos = left.rfind('[')
-            before = left[:brack_pos]
-            text = left[brack_pos + 1:]
-            link, after = right.split(')', 1)
-            s = '%s<a href="%s">%s</a>%s' % (before, link, text, after)
-    except Exception as e:
-        print(e, 'illegal line:', s)
-    return s
-
 if __name__ == '__main__':
     main()
+    sys.exit(0)
