3176171d0d8373145542ad5c72180ef3dad2d281
秦Daniel <daniel_chin@yahoo.com>
2019-09-20 11:01:49 -0400

    Auto upload

--- nyu_ip/entries.py
+++ nyu_ip/entries.py
@@ -1,11 +1,13 @@
 GRAMERCY = 'Gramercy Green'
 STERN = 'Stern (KMC)'
 TANDON = 'Tandon'
+COURANT = 'Courant'
 
 LENOVO = 'Lenovo TianYi'
 
 entries = {
     '10.20.59.227': [GRAMERCY, '2019/9/3', LENOVO], 
     '10.16.219.40': [STERN, '2019/9/3', LENOVO], 
-    '10.18.173.128': [TANDON], '2019/9/11', 'LENOVO', 
+    '10.18.173.128': [TANDON, '2019/9/11', 'LENOVO'], 
+    '10.16.28.27': [COURANT, '2019/9/20', 'LENOVO'], 
 }
