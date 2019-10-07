828d2417e0932d4a4c2fae3002e4ae4060b92b33
Daniel Chin <daniel_chin@yahoo.com>
2019-10-07 10:00:26 -0400

    fix key_codes for POSIX

--- interactive/key_codes.py
+++ interactive/key_codes.py
@@ -1,6 +1,8 @@
 import platform
 
 ESC = b'\x1b\x1b'
+CTRL_C = b'\x03'    
+# On Linux, stdin.read does not actually give ^C, but raises KeyboardInterrupt
 if platform.system().lower() == 'windows':
     WIN_Z_LINUX_D = b'\x1a'
     BACKSPACE = b'\x08'
@@ -15,19 +17,17 @@ if platform.system().lower() == 'windows':
     CTRL_RIGHT = b'\xe0t'
     CTRL_BACKSPACE = b'\x7f'
     CTRL_DELETE =  b'\xe0\x93'
-    CTRL_C = b'\x03'
 else:
     WIN_Z_LINUX_D = b'\x04'
     BACKSPACE = b'\x7f'
     DEL = b'\x1b[3~'
-    UP = b'\xe0H'
-    DOWN = b'\xe0P'
-    LEFT = b'\xe0K'
-    RIGHT = b'\xe0M'
-    HOME = b'\xe0G'
-    END = b'\xe0O'
-    CTRL_LEFT = b'\xe0s'
-    CTRL_RIGHT = b'\xe0t'
-    CTRL_BACKSPACE = b'\x7f'
-    CTRL_DELETE =  b'\xe0\x93'
-    CTRL_C = b'\x03'
+    UP = b'\x1b[A'
+    DOWN = b'\x1b[B'
+    LEFT = b'\x1b[D'
+    RIGHT = b'\x1b[C'
+    HOME = b'\x1b[H'
+    END = b'\x1b[F'
+    CTRL_LEFT = b'\x1b[1;5D'
+    CTRL_RIGHT = b'\x1b[1;5C'
+    CTRL_BACKSPACE = b'DOES NOT SUPPORT'
+    CTRL_DELETE =  b'\x1b[3;5~'
