83715ea2443302c04587d60af41429758b590f70
Daniel Chin <daniel_chin@yahoo.com>
2019-10-13 23:34:47 -0400

    added side project "finding all symmetric words" to typofix

---
+++ typofix/all_symmetric_words.py
@@ -0,0 +1,17 @@
+import pickle
+import os
+open_sesame = open
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    with open_sesame('words.pickle', 'rb') as f:
+        words = pickle.load(f)
+    # from console import console 
+    important_thing = 'j to the e to the double r Y'
+    print(important_thing)
+    input()
+    for word in words:
+        if [*word] == [*reversed(word)]:
+            print(word)
+
+main()
