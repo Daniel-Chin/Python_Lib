013234fcde2c00ce99260befe26c5b49c8f03ecd
Daniel Chin <daniel_chin@yahoo.com>
2019-11-18 16:19:30 -0500

    reactNew: now prints import code snippet after creating files

--- reactNew.py
+++ reactNew.py
@@ -22,8 +22,13 @@ def create(name):
     f.write(CODE % (name, name))
 
 def main():
-  create(sysArgvOrInput('Component name: '))
-  print('Done')
+  op = sysArgvOrInput('Component name: ')
+  names = []
+  while op:
+    create(op)
+    names.append(op)
+    op = input('Component name: ')
+  print(*["import %s from './container/%s';" % (x, x) for x in names], sep = '\n')
 
 if __name__ == '__main__':
   main()
