8d23727eee5284c98e5b3a6d7b77dad01c1de114
Daniel Chin <daniel_chin@yahoo.com>
2021-01-06 14:46:13 +0800

    generalize tag matching

--- cleanWordHtml.py
+++ cleanWordHtml.py
@@ -8,7 +8,7 @@ from os import path
 import shutil
 from myfile import sysArgvOrInput
 
-MSO_9_START = '<!--[if gte mso '
+MSO_9_START = '<!--[if '
 MSO_9_END = '<![endif]-->'
 
 def main():
@@ -16,26 +16,34 @@ def main():
     with TemporaryFile() as tmp:
         with open(filename, 'r', encoding='gb18030') as f:
             during_mso_9 = False
-            for line in f:
-                if not during_mso_9 and line.startswith(
-                    MSO_9_START
-                ):
-                    during_mso_9 = True
-                    # print(1)
-                if during_mso_9 and line.rstrip().endswith(
-                    MSO_9_END
-                ):
-                    during_mso_9 = False
-                    # print(0)
-                    continue
-                if during_mso_9:
-                    continue
-                if MSO_9_START in line or MSO_9_END in line:
-                    print(line)
-                    raise Exception('Error q3490626')
-                line = line.replace('gb2312', 'utf-8')
-                line = line.replace('“', '&quot;').replace('”', '&quot;')
-                tmp.write(line.encode('utf-8'))
+            remains = ''
+            try:
+                while True:
+                    if remains:
+                        line = remains
+                        remains = ''
+                    else:
+                        line = next(f)
+                    if during_mso_9:
+                        parts = line.split(MSO_9_END, 1)
+                        if len(parts) == 2:
+                            line, remains = parts
+                            during_mso_9 = False
+                        continue
+                    else:
+                        parts = line.split(MSO_9_START, 1)
+                        if len(parts) == 2:
+                            line, remains = parts
+                            during_mso_9 = True
+                    if MSO_9_START in line or MSO_9_END in line:
+                        print(line)
+                        raise Exception('Error q3490626')
+                    line = line.replace('gb2312', 'utf-8')
+                    line = line.replace('“', '&quot;')
+                    line = line.replace('”', '&quot;')
+                    tmp.write(line.encode('utf-8'))
+            except StopIteration:
+                pass
         tmp.seek(0)
         with open(filename, 'wb') as f:
             shutil.copyfileobj(tmp, f)
