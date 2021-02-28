8f9c1c1775abfc232fdfd4817c300069549c3397
Daniel Chin <daniel_chin@yahoo.com>
2021-02-28 13:00:43 +0800

    single linked list reversal

--- Archive/algo_exercises_with_ian/reverseLL.py
+++ Archive/algo_exercises_with_ian/reverseLL.py
@@ -1,6 +1,16 @@
-def revLL(head):
+def revDBLL(head):
     cursor = head
     while cursor is not None:
         cursor.prev, cursor.next = cursor.next, cursor.prev
         cursor = cursor.prev
     return cursor.next
+
+def revLL(head):
+    cursor = head
+    last = None
+    while cursor is not None:
+        future = cursor.next
+        cursor.next = last
+        last = cursor
+        cursor = future
+    return last
