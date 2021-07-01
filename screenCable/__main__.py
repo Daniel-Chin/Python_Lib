7854181fcb00f5646d58a1a6053029fab42572cc
Daniel Chin <daniel_chin@yahoo.com>
2021-07-01 11:01:03 +0800

    screenCable

---
+++ screenCable/__main__.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+from mss import mss
+
+RECT = {
+    'left': 585, 'top': 200, 
+    'width': 1195, 'height': 675, 
+}
+
+try:
+    with mss() as sct:
+        while True:
+            cv2.imshow(
+                'screenCable', 
+                np.array(sct.grab(RECT)), 
+            )
+            if cv2.waitKey(33) & 0xFF in (
+                ord('q'), 
+                27, 
+            ):
+                raise KeyboardInterrupt
+except KeyboardInterrupt:
+    print('exiting...')
+finally:
+    cv2.destroyAllWindows()
+    print('ok')
