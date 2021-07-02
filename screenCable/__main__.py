6a8f12d359681e3daab8b0d6dfdff82afcab510c
Daniel Chin <daniel_chin@yahoo.com>
2021-07-02 11:16:20 +0800

    perfection

--- screenCable/__main__.py
+++ screenCable/__main__.py
@@ -12,48 +12,62 @@ import cv2
 from mss import mss
 from time import sleep
 
-def mouseEvent(event, x, y, flags, param):
-    if flags == cv2.EVENT_FLAG_LBUTTON:
-        rect['width'] = x
-        rect['height'] = y
-
-def initWindow():
-    cv2.namedWindow(WINDOW)
-    cv2.setMouseCallback(WINDOW, mouseEvent)
-    cv2.moveWindow(WINDOW, rect['left'], rect['top'])
-
+drag_state = None
 rect = {
     'left': 585, 'top': 200, 
     'width': 1195, 'height': 675, 
 }
-just_refreshed = False
-try:
-    initWindow()
-    with mss() as sct:
+
+def mouseEvent(event, x, y, flags, param):
+    global drag_state
+    if flags == cv2.EVENT_FLAG_LBUTTON:
+        drag_state = 1
+        rect['width'] = x
+        rect['height'] = y
+        cv2.resizeWindow(WINDOW, x, y)
+    if event == cv2.EVENT_LBUTTONUP:
+        drag_state = 2
+
+def main():
+    global drag_state
+    drag_state = 2
+    try:
         while True:
-            cv2.imshow(
-                WINDOW, 
-                np.array(sct.grab(rect)), 
-            )
+            session()
+            sleep(.2)
+            drag_state = 0
+    except KeyboardInterrupt:
+        print('exiting...')
+    finally:
+        cv2.destroyAllWindows()
+        print('ok')
+
+def session():
+    global drag_state
+    with mss() as sct:
+        cv2.imshow(
+            WINDOW, 
+            np.array(sct.grab(rect)), 
+        )
+        cv2.setMouseCallback(WINDOW, mouseEvent)
+        cv2.waitKey(300)
+        while True:
+            if drag_state == 0:
+                cv2.imshow(
+                    WINDOW, 
+                    np.array(sct.grab(rect)), 
+                )
+            key_press = cv2.waitKey(33) & 0xFF
             rect['left'], rect[
                 'top'
             ], _, __ = cv2.getWindowImageRect(WINDOW)
-            key_press = cv2.waitKey(
-                200 if just_refreshed else 33
-            ) & 0xFF
-            just_refreshed = False
             if key_press in (
                 ord('q'), 
                 27, 
             ):
                 raise KeyboardInterrupt
-            elif key_press == ord(' '):
-                cv2.destroyWindow(WINDOW)
-                sleep(.2)
-                initWindow()
-                just_refreshed = True
-except KeyboardInterrupt:
-    print('exiting...')
-finally:
-    cv2.destroyAllWindows()
-    print('ok')
+            elif key_press == ord(' ') or drag_state == 2:
+                break
+    cv2.destroyWindow(WINDOW)
+
+main()
