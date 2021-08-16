df60c602559d94580262159c298c48fa238e6a7e
Daniel Chin <daniel_chin@yahoo.com>
2021-08-16 19:36:48 +0800

    fix

--- editDistance.py
+++ editDistance.py
@@ -4,26 +4,26 @@ DP is implemented via a breadth-first search with function caching.
 '''
 
 from functools import lru_cache
+import numpy as np
 
 def editDistance(
     a, b, 
     insert_cost = 1, del_cost = 1, replace_cost = 1, 
 ):
-    @lru_cache(maxsize=len(a) * len(b))
+    @lru_cache()
     def matrix(x, y):
-        if x == 0:
-            return y * insert_cost
-        if y == 0:
-            return x * del_cost
-        if a[x] == b[y]:
-            return matrix(x - 1, y - 1)
-        else:
-            return min(
-                matrix(x, y - 1) + insert_cost, 
-                matrix(x - 1, y) + del_cost, 
-                matrix(x - 1, y - 1) + replace_cost, 
-            )
-    return matrix(len(a) - 1, len(b) - 1)
+        if x == 0 and y == 0:
+            return 0
+        if x == -1 or y == -1:
+            return np.inf
+        return min(
+            matrix(x - 1, y) + insert_cost, 
+            matrix(x, y - 1) + del_cost, 
+            matrix(x - 1, y - 1) + (
+                0 if a[x - 1] == b[y - 1] else replace_cost
+            ), 
+        )
+    return matrix(len(a), len(b))
 
 def test(a, b, do_swap = True):
     print(f'"{a}", "{b}" ->', editDistance(a, b))
@@ -31,6 +31,7 @@ def test(a, b, do_swap = True):
         test(b, a, False)
 
 if __name__ == '__main__':
+    test('asd', 'rsd')
     test('asdf', 'asdf')
     test('asdf', 'assf')
     test('asdf', 'asf')
@@ -38,4 +39,4 @@ if __name__ == '__main__':
     test('banana palace', 'palace')
     test('cat', 'cut')
     test('saturday', 'sunday')
-    test('1234567890', '123e45ty780')
+    test('1234567890', '23e45ty780')
