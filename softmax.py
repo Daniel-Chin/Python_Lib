7a0b704f798a4cd20a5a9bb10d82b135a0e37aab
Daniel Chin <daniel.chin@nyu.edu>
2022-02-27 11:02:13 -0500

    softmin

--- softmax.py
+++ softmax.py
@@ -7,11 +7,27 @@ from math import log, exp
 COLDNESS = 1
 
 def softmax(X, coldness = COLDNESS):
-    if len(X[0]) == 0:
+    try: 
+        X[0][0]
+    except TypeError:
         X = [(x, 1) for x in X]
     return log(
         sum([
             exp(x * coldness) * prob
+            for (x, prob) in X
+        ]) / sum(
+            [prob for (x, prob) in X]
+        )
+    ) / coldness
+
+def softmin(X, coldness = COLDNESS):
+    try: 
+        X[0][0]
+    except TypeError:
+        X = [(x, 1) for x in X]
+    return - log(
+        sum([
+            exp(-x * coldness) * prob
             for (x, prob) in X
         ]) / sum(
             [prob for (x, prob) in X]
