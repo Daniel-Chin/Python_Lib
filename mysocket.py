68fec544bb4ac9b451a258c45f6cdc8f48760aa9
Daniel Chin <daniel.chin@nyu.edu>
2022-03-31 23:02:02 -0400

    back-incompatible socket upgrade

--- mysocket.py
+++ mysocket.py
@@ -1,10 +1,11 @@
 '''
 My socket utils. Provides `recvall`, `recvFile`, `sendFileJdt`, and `findAPort`.  
 '''
-import jdt
-from socket import socket
 import os
 from os.path import getsize
+from time import time
+from socket import socket, timeout as SocketTimeout
+import jdt
 from pickle_socket import PickleSocket
 from interactive import inputUntilValid, inputChin
 
@@ -34,24 +35,36 @@ def recvFile(s, file_len, to_filename, accept_double_dot = False):
                 j.update(file_len - left)
         j.complete()
 
-def recvall(s, size, use_list = True):
-    '''
-    Receive `size` bytes from socket `s`. Blocks until gets all. 
+def recvall(s: socket, size: int, timeout=10, dt=1):
+    """
+    Receive `size` bytes from socket `s`.  
+    `timeout` is in seconds. If `None`, blocks until gets all. 
     Somehow doesn't handle socket closing. 
     I will fix that when I have time. 
-    '''
-    if use_list:
-        left = size
-        buffer = []
-        while left > 0:
-            buffer.append(s.recv(left))
-            left -= len(buffer[-1])
-        recved = b''.join(buffer)
+    """
+    left = size
+    buffer = []
+    if timeout is None:
+        s.settimeout(None)
     else:
-        recved = b''
-        while len(recved) < size:
-            recved += s.recv(left)
-    return recved
+        deadline = time() + timeout
+        s.settimeout(dt)
+    while left > 0:
+        try:
+            recved = s.recv(left)
+        except SocketTimeout:
+            pass
+        if recved == b'':
+            raise EOFError(f'Socket {s} remote closed. ')
+        buffer.append(recved)
+        left -= len(recved)
+        if (
+            timeout is not None 
+            and time() > deadline 
+            and left > 0
+        ):
+            raise TimeoutError
+    return b''.join(buffer)
 
 def sendFileJdt(s, file, msg = 'send'):
     assert type(s) is socket
