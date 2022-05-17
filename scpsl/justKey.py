a94c06e71f6232c1cf4366d6f06cb50b735405f0
Daniel Chin <daniel.chin@nyu.edu>
2022-05-17 17:02:08 -0400

    verbose

--- scpsl/justKey.py
+++ scpsl/justKey.py
@@ -8,16 +8,20 @@ def onKey(x):
     if x.name == '0' and x.event_type == 'down':
         if is_q_down:
             kb.release('q')
+            print('q ^')
         else:
             kb.press('q')
+            print('q')
         is_q_down = not is_q_down
     if x.name == 'q' and x.event_type == 'up':
         is_q_down = False
     if x.name == '=' and x.event_type == 'down':
         if is_v_down:
             kb.release('v')
+            print('v ^')
         else:
             kb.press('v')
+            print('v')
         is_v_down = not is_v_down
     if x.name == 'v' and x.event_type == 'up':
         is_v_down = False
