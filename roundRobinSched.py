b0aeb7cf991172682d95fa38e6d031798d3400aa
Daniel Chin <daniel.chin@nyu.edu>
2022-06-17 09:06:32 +0400

    upgrade

--- roundRobinSched.py
+++ roundRobinSched.py
@@ -1,16 +1,15 @@
 '''
 Time-based round robin scheduling.  
 '''
-from typing import List, Callable
 from time import perf_counter
 import numpy as np
 
-def roundRobinSched(tasks: List[Callable]):
-    ages = np.zeros((len(tasks), ))
+def roundRobinSched(n_workers):
+    ages = np.zeros((n_workers, ))
     while True:
         elected = ages.argmin()
         start = perf_counter()
-        tasks[elected]()
+        yield elected
         end   = perf_counter()
         ages[elected] += end - start
 
@@ -18,14 +17,10 @@ if __name__ == '__main__':
     from time import sleep
     from random import random
     tasks = []
-    class Task:
-        def __init__(self, t):
-            self.t = t
-        
-        def __call__(self):
-            sleep(self.t)
-            print('slept for', self.t)
     for i in range(2):
         t = random() * .1
-        tasks.append(Task(t))
-    roundRobinSched(tasks)
+        tasks.append(t)
+    for i in roundRobinSched(len(tasks)):
+        t = tasks[i]
+        sleep(t)
+        print('slept for', t)
