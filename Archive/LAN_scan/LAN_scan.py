faff2d0293739e1e547c5219318714a98ea82cb3
Daniel Chin <daniel.chin@nyu.edu>
2022-11-29 11:42:23 +0400

    support ssh scan

--- Archive/LAN_scan/LAN_scan.py
+++ Archive/LAN_scan/LAN_scan.py
@@ -1,39 +1,69 @@
 import sys
-from forcemap import forceMap
-from console import console
 from os import system as cmd
-from pickle_socket import PickleSocket
 from time import time
+from socket import socket, timeout as SockTimeout
+from threading import Lock
 
-TIMEOUT = 10
-THREADS = 16
+from tqdm import tqdm
+
+from forcemap import forceMap
+from pickle_socket import PickleSocket
+from console import console
+
+PING_NOT_PORT = False
+PORT = 22
+TIMEOUT = 1
+THREADS = 32
 
 def main():
-    global hosts, result, s
     ss = PickleSocket()
     ss.bind(('localhost', 2333))
     ss.listen(1)
     print('listening...')
-    s, addr = ss.accept()
-    s.shakeHands()
-    result = forceMap(ping, range(256**2), THREADS)
+    displaySock, addr = ss.accept()
+    displaySock.shakeHands()
+    with tqdm(total=256**2) as pbar:
+        # progress = 0
+        lock = Lock()
+        def acc():
+            # nonlocal progress
+            with lock:
+                # progress += 1
+                pbar.update(1)
+        result = forceMap(
+            lambda i : ping(displaySock, acc, i), 
+            range(256**2), THREADS, 
+        )
     hosts = [x for x in result if x is not None]
     cmd('cls')
     print('There are:', len(hosts))
     console(globals())
 
-def ping(i):
-    ip = '10.209'
+def ping(displaySock: PickleSocket, acc, i):
+    # ip = '10.209'
+    ip = '192.168'
     ip += '.' + str(i // 256)
     ip += '.' + str(i % 256)
-    start = time()
-    cmd('ping ' + ip)
-    if time() - start < TIMEOUT:
-        s.sendObj((True, ip))
-        return ip
+
+    if PING_NOT_PORT:
+        start = time()
+        cmd('ping ' + ip)
+        if time() - start < TIMEOUT:
+            is_up = True
+        else:
+            is_up = False
     else:
-        s.sendObj((False, ip))
-        return None
+        s = socket()
+        s.settimeout(TIMEOUT)
+        try:
+            s.connect((ip, PORT))
+        except (SockTimeout, ConnectionRefusedError, OSError):
+            is_up = False
+        else:
+            is_up = True
+    displaySock.sendObj((is_up, ip))
+    acc()
+    return ip if is_up else None
 
 main()
 sys.exit(0)
