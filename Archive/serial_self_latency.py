a8aa1697ae92e3141a3a5d54ffcd939f995a3a8f
Daniel Chin <daniel.chin@nyu.edu>
2023-05-04 13:50:27 +0800

    serial latency

---
+++ Archive/serial_self_latency.py
@@ -0,0 +1,23 @@
+'''
+Measures the serial self-playback round trip time.  
+'''
+
+import os
+from time import perf_counter
+
+import serial
+
+PORT = 'COM9'
+
+def main():
+    with serial.Serial(PORT, 9600, timeout=1) as s:
+        print('\n\nSerial opened:', s.name)
+        while True:
+            r = os.urandom(1)
+            start = perf_counter()
+            s.write(r)
+            assert s.read(1) == r
+            stop = perf_counter()
+            print(stop - start)
+
+main()
