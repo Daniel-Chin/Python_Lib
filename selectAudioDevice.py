95d730076a974e1a71936bdc575e5d29ae0b8a39
Daniel Chin <daniel.chin@nyu.edu>
2023-10-17 21:16:59 +0400

    tighter import check

--- selectAudioDevice.py
+++ selectAudioDevice.py
@@ -6,9 +6,12 @@ try:
     from interactive import inputChin
 except ImportError as e:
     module_name = str(e).split('No module named ', 1)[1].strip().strip('"\'')
-    print(f'Missing module {module_name}. Please download at')
-    print(f'https://github.com/Daniel-Chin/Python_Lib/blob/master/{module_name}.py')
-    input('Press Enter to quit...')
+    if module_name in (
+        'interactive', 
+    ):
+        print(f'Missing module {module_name}. Please download at')
+        print(f'https://github.com/Daniel-Chin/Python_Lib/blob/master/{module_name}.py')
+        input('Press Enter to quit...')
     raise e
 
 def selectAudioDevice(
