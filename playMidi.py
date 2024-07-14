ad6c16a4ffdf67027dc4eb42256f5217d3aaffbc
Daniel Chin <daniel.chin@nyu.edu>
2024-07-14 11:22:34 +0800

    make interface API instead of Source Code Interface

--- playMidi.py
+++ playMidi.py
@@ -4,27 +4,36 @@ Uses mido to play a midi file.
 
 from __future__ import annotations
 
+import typing as tp
+
 import mido
 from myfile import sysArgvOrInput
 from interactive import inputChin
 
-VELOCITY_SCALE = 1
-
-def channelMap(x: int):
+def any2zero(x: int):   # an example channel remap
     return 0
 
-# def channelMap(x: int):
-#     if x == 3:
-#         return 0
-#     return None
+def identity(x: int):
+    return x
 
-def main():
-    filename = sysArgvOrInput()
-    outputs = mido.get_output_names()
+def askOutput():
+    outputs = mido.get_output_names()   # type: ignore
     for i, name in enumerate(outputs):
         print(i, name, sep = '\t')
-    port_name = outputs[int(inputChin('> ', 0))]
-    with mido.open_output(port_name) as port:
+    return outputs[int(inputChin('> ', '0'))]
+
+def main(
+    filename: str | None = None, 
+    midi_output_name: str | None = None,
+    channel_remap: tp.Callable[[int], int | None] = identity,
+    scale_velocity: float = 1.0, 
+):
+    '''
+    `channel_remap`: return None to discard message.  
+    '''
+    filename = filename or sysArgvOrInput()
+    midi_output_name = midi_output_name or askOutput()
+    with mido.open_output(midi_output_name) as port:    # type: ignore
         down_keys = set()
         with mido.MidiFile(filename) as mid:
             print('playing...')
@@ -32,10 +41,11 @@ def main():
                 for msg in mid.play():
                     print(msg)
                     if isinstance(msg, mido.MetaMessage):
+                        print('Skipping meta message:', msg)
                         continue
                     try:
-                        msg.velocity = round(msg.velocity * VELOCITY_SCALE)
-                        new_channel = channelMap(msg.channel)
+                        msg.velocity = round(msg.velocity * scale_velocity)
+                        new_channel = channel_remap(msg.channel)
                         if new_channel is None:
                             continue
                         msg.channel = new_channel
