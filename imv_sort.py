cb05e4353230a1ec90eafb1a766688bf0d6e0133
Daniel Chin <daniel.chin@nyu.edu>
2024-10-12 01:15:23 +0400

    rando

--- imv_sort.py
+++ imv_sort.py
@@ -3,14 +3,29 @@ Sort files and pass them to imv.
 '''
 
 import os
+import argparse
+import random
 
-def main():
+def main(is_random=False):
     files = os.popen(
         'ls -1t', 
     ).read().strip().split('\n')
+    if is_random:
+        random.shuffle(files)
     command = f'imv {" ".join(files)}'
     print(command)
     os.system(command)
 
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-r', '--random', 
+        action='store_true', 
+        help='Randomize the order of the files',
+    )
+    args = parser.parse_args()
+    return args
+
 if __name__ == '__main__':
-    main()
+    args = parseArgs()
+    main(is_random=args.random)
